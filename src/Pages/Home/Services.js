e8eebda29b9cfd3473181e32d1cef7943595b38e
sayeef-rahman <sayeef.cse@gmail.com>
2022-05-16 22:49:12 +0600

    Services Finished

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import cavity from '../../assets/images/cavity.png'
+import fluoride from '../../assets/images/fluoride.png'
+import whitening from '../../assets/images/whitening.png'
+import Service from './Service';
 
 const Services = () => {
-    const services =[{
-        _id: 01,
-        name: "Fluoride Treatment",
-        description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
-        img:
-    }
+    const services = [
+        {
+            _id: 1,
+            name: "Fluoride Treatment",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: cavity
+        },
+        {
+            _id: 2,
+            name: "Fluoride Treatment",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: fluoride
+        },
+        {
+            _id: 3,
+            name: "Fluoride Treatment",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: whitening
+        }
     ]
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <figure class="px-10 pt-10">
-                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
+        <div>
+            <div className='text-center mt-20'>
+                <p className='text-primary font-bold text-xl'>OUR SERVICES</p>
+                <p className='text-3xl'>Services We Provide</p>
             </div>
-        </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 mx-auto'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    >
+                    </Service>
+                    )
+                }
+            </div>
+        </div >
     );
 };
 
