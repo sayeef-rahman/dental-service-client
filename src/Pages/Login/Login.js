c36277989014934efd4349f20fa730d9b469040f
sayeef-rahman <sayeef.cse@gmail.com>
2022-05-18 21:12:35 +0600

    Home Page & Login Updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>This is Login Page</h1>
+            <h1>This is Login page</h1>
         </div>
     );
 };
