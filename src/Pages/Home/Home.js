7b8e03de5495dd677c157c06a56a52c9d406bfdb
sayeef-rahman <sayeef.cse@gmail.com>
2022-05-18 21:04:18 +0600

    Footer Added & Homeage Finished

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import Contact from './Contact';
 import Info from './Info';
@@ -17,6 +18,7 @@ const Home = () => {
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
             <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
